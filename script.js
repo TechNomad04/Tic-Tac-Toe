2a44d6bd77909467fccf850ba2abd03e30904736
technomad04 <agarwalsamiksha88@gmail.com>
2025-03-07 14:24:13 +0530

    Added the function to enter the markers non repeatedly

--- script.js
+++ script.js
@@ -9,6 +9,18 @@ function createUser(name, marker){
 }
 
 function start(){
-    let user1 = createUser('Samiksha', 'X');
-    let user2 = createUser('Agarwal', 'O');
+    let user1 = new createUser('Samiksha', 'X');
+    let user2 = new createUser('Agarwal', 'O');
+    play(user1, user2);
 }
+
+function play(user1, user2){
+    let x = prompt("Enter x: ");
+    let y = prompt("Enter y: ");
+    if(arr[x][y] !== '')
+        return play(user1, user2);
+    user1.mark(x,y);
+    console.log(arr);
+    play(user2, user1);
+}
+start();
